b8ebd8070f4f9d1fa0fa6413a8ced31dd061c556
juniordev203 <ttk11092k3@gmail.com>
2026-03-25 21:50:41 +0700

    feat: add husky, eslint, prettier, shadcn ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
+    },
+  },
 ]);
 
 export default eslintConfig;
